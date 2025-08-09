39732cd72d9686088717dbfd2bead9fb314025e7
israel <ioluyomi@gmail.com>
2025-08-09 08:15:05 +0100

    updated for chrome

--- src/main/java/utils/WebDriverManager.java
+++ src/main/java/utils/WebDriverManager.java
@@ -3,6 +3,8 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverManager {
     private static WebDriver driver;
@@ -10,10 +12,12 @@ public class WebDriverManager {
     public static WebDriver getDriver() {
         if (driver == null) {
             // Optional: Add any Edge options if needed
-            EdgeOptions options = new EdgeOptions();
+           // EdgeOptions options = new EdgeOptions();
+            ChromeOptions options = new ChromeOptions();
             // options.addArguments("--headless");  // Uncomment this line to run in headless mode
 
-            driver = new EdgeDriver(options);
+           // driver = new EdgeDriver(options);
+            driver = new ChromeDriver(options);
         }
         return driver;
     }
